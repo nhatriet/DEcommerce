e79510e7d59b966a1f6bed8a1a2be83f658bab37
nhatriet <nhatriet2311@gmail.com>
2023-12-16 16:30:54 +0700

    init

---
+++ DEcommerce/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // apiUrl: "http://localhost:38714/api/",
+  // hostImage: "http://localhost:38714/file/",
+  apiUrl: "http://localhost:5000/api/",
+  hostImage: "http://localhost:5000/file/",
+};
